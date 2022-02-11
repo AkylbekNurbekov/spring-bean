a218c1f56c875272e636f499a45c4538ba55bd76
AkylbekNurbekov <97018017+AkylbekNurbekov@users.noreply.github.com>
2022-02-11 21:52:49 +0600

    lesson13

--- src/main/java/peaksoft/App.java
+++ src/main/java/peaksoft/App.java
@@ -2,13 +2,22 @@ package peaksoft;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import peaksoft.model.Cat;
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        System.out.println(bean1.getMessage());
+        System.out.println(bean1 == bean2);
+
+        Cat cat1 = applicationContext.getBean("cat",Cat.class);
+        Cat cat2 = applicationContext.getBean("cat",Cat.class);
+
+        System.out.println(cat1 == cat2);
+
     }
 }
